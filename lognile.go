a5de576fc15105ed4e0cc8208a3770733b0faed1
lee <isme@live.com>
2024-01-19 16:57:25 +0800

    进度读取与保存

--- lognile.go
+++ lognile.go
@@ -2,6 +2,7 @@ package main
 
 import "os"
 import "log"
+import "encoding/json"
 import "gopkg.in/yaml.v3"
 
 func main() {
@@ -11,10 +12,16 @@ func main() {
 	log.Println("ok")
 }
 
-type Lognile struct {}
+type Lognile struct {
+	offset map[string]int64
+}
 
 func (L *Lognile) Init() {
-	log.Println(L.config("config.yaml"))
+	//log.Println(L.config("config.yaml"))
+	L.offset = L.load("log.db")
+	log.Println(L.offset)
+	L.offset["a"] = 123
+	L.save("log.db")
 }
 
 func (L *Lognile) config(cfg string) map[string]any {
@@ -30,4 +37,38 @@ func (L *Lognile) config(cfg string) map[string]any {
     }
 
     return data
+}
+
+func (L *Lognile) load(db string) map[string]int64 {
+	_, err := os.Stat(db)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return map[string]int64{}
+		}
+		log.Fatal(err)
+	}
+
+	content, err := os.ReadFile(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var offset map[string]int64
+	err = json.Unmarshal(content, &offset)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return offset
+}
+
+func (L *Lognile) save(db string) {
+	content, err := json.Marshal(L.offset)
+	if err != nil {
+		log.Fatal(err)
+    }
+
+    if err := os.WriteFile(db, []byte(content), 0666); err != nil {
+        log.Fatal(err)
+    }
 }
