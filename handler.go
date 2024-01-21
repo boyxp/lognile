8023b3d6d6315fa4fded53c19122253838292e17
lee <isme@live.com>
2024-01-21 14:10:38 +0800

    单独拆分

---
+++ handler.go
@@ -0,0 +1,21 @@
+package lognilen
+
+import "os"
+import "sync"
+
+type Handler struct {
+	pointer *os.File
+	mu sync.Mutex
+}
+
+func (H *Handler) Lock() bool {
+	return H.mu.TryLock()
+}
+
+func (H *Handler) Unlock() {
+	H.mu.Unlock()
+}
+
+func (H *Handler) Pointer() *os.File {
+	return H.pointer
+}
